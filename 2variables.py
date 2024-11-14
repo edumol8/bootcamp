b8c988789e722d60f1a8660da1de075c2d6fbbb2
eduardo molina <edumolinzam@gmail.com>
2024-11-13 23:52:01 -0500

    hola

---
+++ 2variables.py
@@ -0,0 +1,14 @@
+"""
+nombre = "Hola Mundo"
+print(nombre)
+
+#variable :referencia en memoria que alamacena info
+
+numero = 17
+print(type(numero), type(nombre))"""
+
+nombre = "Diego"
+numero = 17
+peso = 36.17
+mayorEdad = True 
+print(type(mayorEdad))
