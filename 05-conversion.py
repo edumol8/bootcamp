0689c986b8464bbb8fba73f14e762502c03d6b04
eduardo molina <edumolinzam@gmail.com>
2024-11-20 00:15:18 -0500

    5

---
+++ 05-conversion.py
@@ -0,0 +1,6 @@
+edad = int (input("cual es su edad actual?: "))
+
+print("su edad actual es: "+ str(edad))
+
+print ("su edad fue hace 5 años de: "+ str(edad-5))
+print ("su edad fue hace 5 años de: ", edad-5)
